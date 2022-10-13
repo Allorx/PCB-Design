9df710f07f31ec4ca4cacaa3ae2fb7aaf218b3c8
Allorx <31127228+Allorx@users.noreply.github.com>
2022-10-13 22:50:24 +0100

    added cpp keyboard-code
    + includes cmake files and config

---
+++ keyboard-code/build/CMakeFiles/orionshands.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orionshands.dir/src/main.cpp.obj"
+  "orionshands.exe"
+  "orionshands.exe.manifest"
+  "orionshands.lib"
+  "orionshands.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orionshands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ keyboard-code/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Documents/Github/PCB-Design/keyboard-code
+# Build directory: D:/Documents/Github/PCB-Design/keyboard-code/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
