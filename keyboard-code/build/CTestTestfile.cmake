131e6d6eef65ae97b826e783079c952797087e11
Allorx <31127228+Allorx@users.noreply.github.com>
2022-10-14 00:30:48 +0100

    cleaned up directory
    - misconfigured so restarting

--- keyboard-code/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/Github/PCB-Design/keyboard-code
-# Build directory: D:/Documents/Github/PCB-Design/keyboard-code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
