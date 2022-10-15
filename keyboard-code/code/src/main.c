cb6409f9c138dd363544cba5b77c722fed729521
Allorx <31127228+Allorx@users.noreply.github.com>
2022-10-15 18:24:10 +0100

    updated cmakelists and added to main.c
    + mostly just cleaning up and testing things out

--- keyboard-code/code/src/main.c
+++ keyboard-code/code/src/main.c
@@ -1,16 +1,34 @@
+// Aleksas Girenas 15/10/2022
+// For controlling OrionsHands (a fully custom keyboard)
+
 #include "pico/stdlib.h"
 
-#define LED_PIN PICO_DEFAULT_LED_PIN
-#define CLM_0
+// keyboard rotary encoder inputs
+#define CLK 0
+#define DT 1
 
 int main() {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // keyboard columns starting from COL_0, COL_1, ...
+    uint cols[] = {13,14,15,12,11,10,9,8,2,3,4,5,6,7};
+    // keyboard rows starting from ROW_0, ROW_2,...
+    uint rows[] = {20,19,18,17,16};
+    // init gpio pins and set directions
+    for(int i = 0; i < 14; i++){
+        gpio_init(cols[i]);
+        gpio_set_dir(cols[i], true);
+    }
+    for(int i = 0; i < 5; i++){
+        gpio_init(rows[i]);
+    }
 
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        for(int i = 0; i < 14; i++){
+            // send signal from pin i
+            gpio_put(cols[i], 1);
+            // read row pins
+
+            // turn off signal from pin i
+            gpio_put(cols[i], 0);
+        }
     }
 }
