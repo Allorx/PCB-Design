9663ac63913b035ccac1978ee5998d0904e3e864
Allorx <31127228+Allorx@users.noreply.github.com>
2022-10-15 22:59:46 +0100

    added keyboard matrix logic and debounce
    * need to relate key_out to key values now and send through HID usb

--- keyboard-code/code/src/main.c
+++ keyboard-code/code/src/main.c
@@ -6,29 +6,75 @@
 // keyboard rotary encoder inputs
 #define CLK 0
 #define DT 1
+// define key presses
+#define KEY_PRESSED 0
+#define KEY_RELEASED 1
 
 int main() {
-    // keyboard columns starting from COL_0, COL_1, ...
-    uint cols[] = {13,14,15,12,11,10,9,8,2,3,4,5,6,7};
-    // keyboard rows starting from ROW_0, ROW_2,...
-    uint rows[] = {20,19,18,17,16};
+    // define key outputs
+    uint key_out[14][5] = {KEY_RELEASED};
+    // define key debounce counter array and number of checks until a confirmed press
+    int key_debounce[14][5] = {0};
+    uint confirmed_press = 10;
+
+    // gpio pin nums of keyboard columns starting from COL_0, COL_1, ...
+    uint cols[14] = {13,14,15,12,11,10,9,8,2,3,4,5,6,7};
+    // gpio pin nums of keyboard rows starting from ROW_0, ROW_2,...
+    uint rows[5] = {20,19,18,17,16};
+
     // init gpio pins and set directions
     for(int i = 0; i < 14; i++){
         gpio_init(cols[i]);
-        gpio_set_dir(cols[i], true);
+        gpio_set_dir(cols[i], false);
+        gpio_pull_up(cols[i]);
     }
     for(int i = 0; i < 5; i++){
         gpio_init(rows[i]);
+        gpio_set_dir(rows[i], false);
+        gpio_pull_up(rows[i]);
     }
 
     while (true) {
         for(int i = 0; i < 14; i++){
-            // send signal from pin i
-            gpio_put(cols[i], 1);
-            // read row pins
+            // send signal from column i
+            gpio_set_dir(cols[i], true);
+            gpio_put(cols[i], false);
 
-            // turn off signal from pin i
-            gpio_put(cols[i], 0);
+            for(int j = 0; j < 5; j++){
+                // read row j: if it is 0 and the debounce on the key exceeds confirmed_press, then i,j is pressed
+                bool read = gpio_get(rows[j]);
+                if(read == KEY_PRESSED && key_out[i][j] == KEY_RELEASED){
+                    if(key_debounce[i][j] > confirmed_press){
+                        // key has been confirmed to be pressed
+                        key_out[i][j] = KEY_PRESSED;
+                        key_debounce[i][j] = 0;
+                    }
+                    else{
+                        key_debounce[i][j] ++;
+                    }
+                }
+                else if(read == KEY_RELEASED && key_out[i][j] == KEY_PRESSED){
+                    if(key_debounce[i][j] < -confirmed_press){
+                        // key has been confirmed to be released
+                        key_out[i][j] = KEY_RELEASED;
+                        key_debounce[i][j] = 0;
+                    }
+                    else{
+                        key_debounce[i][j] --;
+                    }
+                }
+                else if(key_debounce[i][j] != 0){
+                    // current read is same as key_out for i,j - return towards 0 debounce counts
+                    if(read == KEY_RELEASED){
+                        key_debounce[i][j] --;
+                    }
+                    else{
+                        key_debounce[i][j] ++;
+                    }
+                }
+            }
+            // turn off signal from column i
+            gpio_set_dir(cols[i], false);
         }
     }
 }
