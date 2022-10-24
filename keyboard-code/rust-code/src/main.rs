352952433f60e72d9ff36a318935a41cfff44cd7
Allorx <31127228+Allorx@users.noreply.github.com>
2022-10-24 21:18:42 +0100

    added usb hid to rust code
    + struggled to implement a loop over gpio pins....
    * still need to learn rust fully

--- keyboard-code/rust-code/src/main.rs
+++ keyboard-code/rust-code/src/main.rs
@@ -1,37 +1,35 @@
 // Aleksas Girenas 23/10/2022
 // For controlling OrionsHands (a fully custom keyboard)
+// core code taken from https://github.com/dlkj/usbd-human-interface-device/blob/main/examples/src/bin/keyboard_nkro.rs
 
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
-use panic_halt as _;
-use rp2040_hal as hal;
-
+use bsp::entry;
+use bsp::hal;
+use bsp::hal::gpio::dynpin;
+use core::convert::Infallible;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::v2::*;
+use embedded_hal::prelude::*;
+use fugit::ExtU32;
 use hal::pac;
+use panic_probe as _;
+use usb_device::class_prelude::*;
+use usb_device::prelude::*;
+use usbd_human_interface_device::page::Keyboard;
+use usbd_human_interface_device::prelude::*;
 
-use embedded_hal::digital::v2::OutputPin;
-use rp2040_hal::clocks::Clock;
+use rp_pico as bsp;
 
-// where to place the boot block
-#[link_section = ".boot2"]
-#[used]
-pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
-
-// crystal freq
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
-// entry point after global variable initialisation
 #[entry]
 fn main() -> ! {
-    // grab our peripherals
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    // create a watchdog
+
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-    // Clock configuration
     let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -41,25 +39,184 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
-    // setting up delay
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().raw());
-    // gpio control with single-cycle io block
-    let sio = hal::Sio::new(pac.SIO);
 
-    // set pins to default state
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+
+    let sio = hal::Sio::new(pac.SIO);
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    // led pin (GPIO25) as output
-    let mut led_pin = pins.gpio25.into_push_pull_output();
-    // main loop
+
+    info!("Starting");
+
+    //USB
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut keyboard = UsbHidClassBuilder::new()
+        .add_interface(
+            usbd_human_interface_device::device::keyboard::NKROBootKeyboardInterface::default_config(),
+        )
+        .build(&usb_bus);
+
+    //https://pid.codes
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+        .manufacturer("Orions Hands")
+        .product("Orions Hands")
+        .serial_number("TEST")
+        .max_packet_size_0(8)
+        .build();
+
+    //GPIO pins
+    //let mut col_pins: [
+        //pins.gpio13.into_push_pull_output(),
+        //&pins.gpio14,
+        //&pins.gpio15,
+        //&pins.gpio12,
+        //&pins.gpio11,
+        //&pins.gpio10,
+        //&pins.gpio9,
+        //&pins.gpio8,
+        //&pins.gpio2,
+        //&pins.gpio3,
+        //&pins.gpio4,
+        //&pins.gpio5,
+        //&pins.gpio6,
+        //&pins.gpio7,
+    //];
+
+    let keys: &[&dyn InputPin<Error = core::convert::Infallible>] = &[
+        &pins.gpio1.into_pull_up_input(),
+        &pins.gpio2.into_pull_up_input(),
+        &pins.gpio3.into_pull_up_input(),
+        &pins.gpio4.into_pull_up_input(),
+        &pins.gpio5.into_pull_up_input(),
+        &pins.gpio6.into_pull_up_input(),
+        &pins.gpio7.into_pull_up_input(),
+        &pins.gpio8.into_pull_up_input(),
+        &pins.gpio9.into_pull_up_input(),
+        &pins.gpio10.into_pull_up_input(),
+        &pins.gpio11.into_pull_up_input(),
+        &pins.gpio12.into_pull_up_input(),
+    ];
+
+    let mut input_count_down = timer.count_down();
+    input_count_down.start(2.millis());
+
+    let mut tick_count_down = timer.count_down();
+    tick_count_down.start(1.millis());
+
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        //Poll the keys every 2ms
+        if input_count_down.wait().is_ok() {
+            let keys = get_keys(keys);
+
+            match keyboard.interface().write_report(&keys) {
+                Err(UsbHidError::WouldBlock) => {}
+                Err(UsbHidError::Duplicate) => {}
+                Ok(_) => {}
+                Err(e) => {
+                    core::panic!("Failed to write keyboard report: {:?}", e)
+                }
+            };
+        }
+
+        //Tick once per ms
+        if tick_count_down.wait().is_ok() {
+            match keyboard.interface().tick() {
+                Err(UsbHidError::WouldBlock) => {}
+                Ok(_) => {}
+                Err(e) => {
+                    core::panic!("Failed to process keyboard tick: {:?}", e)
+                }
+            };
+        }
+
+        if usb_dev.poll(&mut [&mut keyboard]) {
+            match keyboard.interface().read_report() {
+                Err(UsbError::WouldBlock) => {
+                    //do nothing
+                }
+                Err(e) => {
+                    core::panic!("Failed to read keyboard report: {:?}", e)
+                }
+                Ok(leds) => {
+                    //led_pin.set_state(PinState::from(leds.caps_lock)).ok();
+                }
+            }
+        }
     }
+}
+
+fn get_keys(keys: &[&dyn InputPin<Error = Infallible>]) -> [Keyboard; 12] {
+    [
+        if keys[0].is_low().unwrap() {
+            Keyboard::KeypadNumLockAndClear
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[1].is_low().unwrap() {
+            Keyboard::A
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[2].is_low().unwrap() {
+            Keyboard::B
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[3].is_low().unwrap() {
+            Keyboard::C
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[4].is_low().unwrap() {
+            Keyboard::D
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[5].is_low().unwrap() {
+            Keyboard::E
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[6].is_low().unwrap() {
+            Keyboard::F
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[7].is_low().unwrap() {
+            Keyboard::G
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[8].is_low().unwrap() {
+            Keyboard::H
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[9].is_low().unwrap() {
+            Keyboard::I
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[10].is_low().unwrap() {
+            Keyboard::J
+        } else {
+            Keyboard::NoEventIndicated
+        },
+        if keys[11].is_low().unwrap() {
+            Keyboard::K
+        } else {
+            Keyboard::NoEventIndicated
+        },
+    ]
 }
