bd8fff7b5924c97b5a61947d3f60215499565462
Allorx <31127228+Allorx@users.noreply.github.com>
2022-11-23 19:35:59 +0000

    added todo comment

--- keyboard-code/rust-code/src/main.rs
+++ keyboard-code/rust-code/src/main.rs
@@ -988,3 +988,4 @@ fn get_fnkeys(keys: [[i32; 14]; 5]) -> [Keyboard; 63] {
 // ? pio could be very good for polling keys but unnecessary - there is more than enough headroom and speed currently - pio could be used for future features
 
 // todo when switch to darkstar systems ltd make new commit for pidcodes
+// todo make use of other core
