d3d6eb12fd92b1fb0469556762ddd736ab3291a9
Allorx <31127228+Allorx@users.noreply.github.com>
2023-11-19 20:01:32 +0000

    setting up pico w with bluetooth
    + still has some issues but wip

---
+++ keyboard-code/code/src/bt_hid.c
@@ -0,0 +1,285 @@
+//? heavily modified from "hid_keyboard_demo.c" by BlueKitchen/BTStack
+/*
+ * Copyright (C) 2014 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BLUEKITCHEN
+ * GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+#include "btstack_defines.h"
+#include "btstack_ring_buffer.h"
+#include "btstack_run_loop.h"
+#include "classic/hid_device.h"
+#ifndef BT_HID_FILE
+#define BT_HID_FILE "bt_hid.c"
+
+#include <stdint.h>
+#include "btstack.h"
+#include "inttypes.h"
+
+#define KEYBOARD_REPORT_ID 0x01
+#define REPORT_BYTES 16 // n = 16 key rollover
+
+//#define REPORT_DELAY_MS 1
+
+// When not set to 0xffff, sniff and sniff subrating are enabled
+//static uint16_t host_max_latency = 1600;
+//static uint16_t host_min_timeout = 3200;
+
+static uint8_t hid_service_buffer[300];
+static uint8_t device_id_sdp_service_buffer[100];
+static const char hid_device_name[] = "Orions Hands";
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+static uint16_t hid_cid;
+static uint8_t hid_boot_device = 0;
+
+// buffers
+static uint8_t key_buffer_storage[16] = {0};
+static uint8_t current_buffer = 0;
+static uint8_t key_modifier = 0;
+
+// States
+static enum {
+    APP_BOOTING,
+    APP_NOT_CONNECTED,
+    APP_CONNECTING,
+    APP_CONNECTED
+} app_state = APP_BOOTING;
+
+// Delay timers
+static btstack_timer_source_t reporting;
+
+// close to USB HID Specification 1.1, Appendix B.1
+const uint8_t hid_descriptor_keyboard[] = {
+    0x05, 0x01,                    // Usage Page (Generic Desktop)
+    0x09, 0x06,                    // Usage (Keyboard)
+    0xa1, 0x01,                    // Collection (Application)
+
+    // Report ID
+    0x85, KEYBOARD_REPORT_ID,               // Report ID
+    // Modifier byte (input)
+    0x95, 0x08,                    //   Report Count (8)
+    0x75, 0x01,                    //   Report Size (1)
+    0x05, 0x07,                    //   Usage Page (Key codes)
+    0x19, 0xe0,                    //   Usage Minimum (Keyboard LeftControl)
+    0x29, 0xe7,                    //   Usage Maximum (Keyboard Right GUI)
+    0x15, 0x00,                    //   Logical Minimum (0)
+    0x25, 0x01,                    //   Logical Maximum (1)
+    0x81, 0x02,                    //   Input (Data, Variable, Absolute)
+    // Reserved byte (input)
+    0x95, 0x08,                    //   Report Count (8)
+    0x75, 0x01,                    //   Report Size (1)
+    0x81, 0x03,                    //   Input (Constant, Variable, Absolute)
+    // LED report (output)
+    0x95, 0x05,                    //   Report Count (5)
+    0x75, 0x01,                    //   Report Size (1)
+    0x05, 0x08,                    //   Usage Page (LEDs)
+    0x19, 0x01,                    //   Usage Minimum (Num Lock)
+    0x29, 0x05,                    //   Usage Maximum (Kana)
+    0x91, 0x02,                    //   Output (Data, Variable, Absolute)
+    // 3 bits reserved padding (output)
+    0x95, 0x01,                    //   Report Count (1)
+    0x75, 0x03,                    //   Report Size (3)
+    0x91, 0x03,                    //   Output (Constant, Variable, Absolute)
+    // Keycodes (input)
+    0x95, (REPORT_BYTES-1)*8-1,    //   Report Count (n)
+    0x75, 0x08,                    //   Report Size (8)
+    0x15, 0x00,                    //   Logical Minimum (0)
+    0x25, 0xff,                    //   Logical Maximum (1)
+    0x05, 0x07,                    //   Usage Page (Key codes)
+    0x19, 0x00,                    //   Usage Minimum (Reserved (no event indicated))
+    0x29, (REPORT_BYTES-1)*8-1,    //   Usage Maximum (n)
+    0x81, 0x00,                    //   Input (Data, Array)
+
+    0xc0,                          // End collection
+};
+
+static void queue_character(uint8_t character){
+    // queue character into an array and stop when reached max limit
+    if(current_buffer < sizeof(key_buffer_storage)){
+        key_buffer_storage[current_buffer] = character;
+        current_buffer += 1;
+    }
+}
+
+static void clear_characters(){
+    memset(key_buffer_storage, 0, sizeof(key_buffer_storage));
+    current_buffer = 0;
+}
+
+static void send_report(){
+    uint8_t report[20] = {0xa1, KEYBOARD_REPORT_ID, key_modifier, 0};
+    memcpy(report + 4, key_buffer_storage, 4 * sizeof(uint8_t));
+    hid_device_send_interrupt_message(hid_cid, &report[0], sizeof(report));
+    clear_characters();
+}
+
+static void report_ready(){
+    if(!hid_cid) return;
+    hid_device_request_can_send_now_event(hid_cid);
+}
+
+//static void reporting_handler(struct btstack_timer_source *ts){
+//    if(!hid_cid) return;
+//    hid_device_request_can_send_now_event(hid_cid);
+//    // set next timer
+//    btstack_run_loop_set_timer(ts, REPORT_DELAY_MS);
+//    btstack_run_loop_add_timer(ts);
+//}
+
+//static void start_reporting(){
+//    reporting.process = &reporting_handler;
+//    btstack_run_loop_set_timer(&reporting, REPORT_DELAY_MS);
+//    btstack_run_loop_add_timer(&reporting);
+//}
+
+// check what state we are at
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t * packet, uint16_t packet_size){
+    UNUSED(channel);
+    UNUSED(packet_size);
+    uint8_t status;
+    switch (packet_type){
+        case HCI_EVENT_PACKET:
+            switch (hci_event_packet_get_type(packet)){
+                case BTSTACK_EVENT_STATE:
+                    if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
+                    app_state = APP_NOT_CONNECTED;
+                    break;
+
+                case HCI_EVENT_USER_CONFIRMATION_REQUEST:
+                    // ssp: inform about user confirmation request
+                    log_info("SSP User Confirmation Request with numeric value '%06"PRIu32"'\n", hci_event_user_confirmation_request_get_numeric_value(packet));
+                    log_info("SSP User Confirmation Auto accept\n");                   
+                    break; 
+
+                case HCI_EVENT_HID_META:
+                    switch (hci_event_hid_meta_get_subevent_code(packet)){
+                        case HID_SUBEVENT_CONNECTION_OPENED:
+                            status = hid_subevent_connection_opened_get_status(packet);
+                            if (status != ERROR_CODE_SUCCESS) {
+                                // outgoing connection failed
+                                //printf("Connection failed, status 0x%x\n", status);
+                                app_state = APP_NOT_CONNECTED;
+                                hid_cid = 0;
+                                return;
+                            }
+                            app_state = APP_CONNECTED;
+                            hid_cid = hid_subevent_connection_opened_get_hid_cid(packet); 
+                            gap_discoverable_control(0); // disable discovery now that we are connected (to save some power)
+                            //start_reporting();
+
+                            break;
+                        case HID_SUBEVENT_CONNECTION_CLOSED:
+                            // HID disconnected
+                            app_state = APP_NOT_CONNECTED;
+                            hid_cid = 0; 
+                            gap_discoverable_control(1); // re-enable discovery
+                            
+                            break;
+                        case HID_SUBEVENT_CAN_SEND_NOW:
+                            send_report();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+int bt_init(){
+    // allow to get found by inquiry
+    gap_discoverable_control(1);
+    // use Limited Discoverable Mode; Peripheral; Keyboard as CoD
+    gap_set_class_of_device(0x2540);
+    // set local name to be identified
+    gap_set_local_name("Orions Hands");
+    // allow for role switch in general and sniff mode
+    gap_set_default_link_policy_settings( LM_LINK_POLICY_ENABLE_ROLE_SWITCH | LM_LINK_POLICY_ENABLE_SNIFF_MODE );
+    // allow for role switch on outgoing connections - this allow HID Host to become master when we re-connect to it
+    gap_set_allow_role_switch(true);
+
+    // L2CAP
+    l2cap_init();
+
+    // SDP Server
+    sdp_init();
+    memset(hid_service_buffer, 0, sizeof(hid_service_buffer));
+
+    uint8_t hid_virtual_cable = 0;
+    uint8_t hid_remote_wake = 1;
+    uint8_t hid_reconnect_initiate = 1;
+    uint8_t hid_normally_connectable = 1;
+
+    hid_sdp_record_t hid_params = {
+        // hid sevice subclass 2540 Keyboard, hid counntry code 33 US
+        0x2540, 33, 
+        hid_virtual_cable, hid_remote_wake, 
+        hid_reconnect_initiate, hid_normally_connectable,
+        hid_boot_device,
+        //host_max_latency, host_min_timeout,
+        0xFFFF, 0xFFFF,
+        3200,
+        hid_descriptor_keyboard,
+        sizeof(hid_descriptor_keyboard),
+        hid_device_name
+    };
+    
+    hid_create_sdp_record(hid_service_buffer, 0x10001, &hid_params);
+    sdp_register_service(hid_service_buffer);
+
+    // bluetooth company identifiers...
+    device_id_create_sdp_record(device_id_sdp_service_buffer, 0x10003, DEVICE_ID_VENDOR_ID_SOURCE_BLUETOOTH, BLUETOOTH_COMPANY_ID_BLUEKITCHEN_GMBH, 1, 1);
+    sdp_register_service(device_id_sdp_service_buffer);
+
+    // HID Device
+    hid_device_init(hid_boot_device, sizeof(hid_descriptor_keyboard), hid_descriptor_keyboard);
+       
+    // register for HCI events
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    // register for HID events
+    hid_device_register_packet_handler(&packet_handler);
+
+    // turn on!
+    hci_power_control(HCI_POWER_ON);
+    return 0;
+}
+
+#endif
--- keyboard-code/code/src/main.c
+++ keyboard-code/code/src/main.c
@@ -1,166 +1,158 @@
 // Aleksas Girenas 15/10/2022
-// For controlling OrionsHands (a fully custom keyboard)
+// OrionsHands Firmware for rp pico w
 
-// #include <stdio.h>
+#include "hardware/gpio.h"
 #include "pico/stdlib.h"
-#include "hid_keyboard_demo.c"
+#include "bt_hid.c"
 #include "pico/cyw43_arch.h"
+#include "pico/multicore.h"
 
+/*
 // keyboard rotary encoder inputs
 #define CLK 0
 #define DT 1
-// define key presses
-#define KEY_PRESSED 0
-#define KEY_RELEASED 1
-// define debounce FLIP and MAX_DEBOUNCE states in cycles
-#define MAX_DEBOUNCE 10
-#define FLIP 5
-// define FUNC_KEY
-#define FUNC_KEY 0xFF
+*/
+
+// for debounce
+#define CONFIRMED_PRESS 5
+// bluetooth reporting rate
+#define REPORT_DELAY_MS 1000
+
+// ?keymaps
+const uint8_t keymap[5][14] =
+{
+    {0x29,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x2D,0x2E,0x2A},
+    {0x2B,0x14,0x1A,0x08,0X15,0X17,0X1C,0X18,0X0C,0X12,0X13,0X2F,0X30,0X48},
+    {0x39,0X04,0X16,0X07,0X09,0X0A,0X0B,0X0D,0X0E,0X0F,0X33,0X34,0X28,0X4C},
+    {0xE1,0X31,0X1D,0X1B,0X06,0X19,0X05,0X11,0X10,0X36,0X37,0X38,0X32,0X52},
+    {0xE0,0XE3,0XE2,0X00,0X00,0X00,0X2C,0X00,0X00,0XE6,0x00,0X50,0X51,0X4F}
+};
+const uint8_t fn_keymap[5][14] =
+{
+    {0x00,0X3A,0X3B,0X3C,0X3D,0X3E,0X3F,0X40,0X41,0X42,0X43,0X44,0X45,0x00},
+    {0x2B,0x14,0x1A,0x08,0X15,0X17,0X1C,0X18,0X0C,0X12,0X13,0X2F,0X30,0X48},
+    {0x39,0X04,0X16,0X07,0X09,0X0A,0X0B,0X0D,0X0E,0X0F,0X33,0X34,0X28,0X4C},
+    {0xE1,0X31,0X1D,0X1B,0X06,0X19,0X05,0X11,0X10,0X36,0X37,0X38,0X32,0X52},
+    {0xE0,0XE3,0XE2,0X00,0X00,0X00,0X2C,0X00,0X00,0XE6,0x00,0X50,0X51,0X4F}
+};
+
+// define key outputs i.e. pressed or released
+uint8_t pressed_keys[5][14] = {0};
+// define key debounce counter array
+int8_t debounce_keys[5][14] = {0};
+// gpio pin nums of keyboard columns starting from COL_0, COL_1, ...
+uint8_t cols[14] = {13,14,15,12,11,10,9,8,2,3,4,5,6,7};
+// gpio pin nums of keyboard rows starting from ROW_0, ROW_2,...
+uint8_t rows[5] = {20,19,18,17,16};
+
+// ?timers
+bool timer_fired = false;
+int64_t report_alarm_callback(alarm_id_t id, void *user_data){
+    // alarm is completed 
+    timer_fired = true;
+    return 0;
+}
+
+void scan_keys(){
+    //? Scan all keys to assign which are pressed/released to pressed_keys
+    for(int col = 0; col < 14; col++){
+        // send signal from this column
+        gpio_set_dir(cols[col], true);
+        gpio_put(cols[col], 0);
+        for(int row = 0; row < 5; row++){
+            // read row row: if it is 0 and the debounce on the key exceeds FLIP, then col,row is pressed
+            if(gpio_get(rows[row]) == 0){
+                if(debounce_keys[row][col] > CONFIRMED_PRESS){
+                    // key has been confirmed to be pressed
+                    pressed_keys[row][col] = 1;
+                    debounce_keys[row][col] = 0;
+                }
+                else{
+                    // increment debounce state
+                    debounce_keys[row][col] ++;
+                }
+            }
+            else {
+                if(debounce_keys[row][col] < -CONFIRMED_PRESS){
+                    // key has been confirmed to be released
+                    pressed_keys[row][col] = 0;
+                    debounce_keys[row][col] = 0;
+                }
+                else{
+                    // decrement debounce state
+                    debounce_keys[row][col] --;
+                }
+            }
+        }
+        // turn off signal from this column
+        gpio_set_dir(cols[col], false);
+    }
+}
+
+void core1_main(){
+    //? testing
+    while(true){
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(1000);
+    }
+}
 
 int main()
 {
+    //? Initialise
     stdio_init_all();
     if (cyw43_arch_init())
     {
-        printf("failed to initialise cyw43_arch\n");
+        // failed to init cyw43
         return -1;
     }
 
-    // bt
-    btstack_main(0, NULL);
-    gap_set_local_name("Orions Hands");
+    bt_init();
+    multicore_launch_core1(core1_main);
+    
+    //? set up report alarm
+    add_alarm_in_ms(REPORT_DELAY_MS, report_alarm_callback, NULL, false);
+
+    //? init gpio pins and set directions
+    for(int col = 0; col < 14; col++){
+        gpio_init(cols[col]);
+        gpio_set_dir(cols[col], false);
+        gpio_pull_up(cols[col]);
+    }
+    for(int row = 0; row < 5; row++){
+        gpio_init(rows[row]);
+        gpio_set_dir(rows[row], false);
+        gpio_pull_up(rows[row]);
+    }
 
     while (true)
-    {
-        uint8_t *text = "hello, world";
-        btstack_ring_buffer_write(&send_buffer, text, strlen(text));
-        send_next(NULL);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(2500);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(2500);
-    }
+    {   
+        scan_keys();
 
-    return 0;
-}
-/*
-    // keymaps
-    const unsigned char keymap[5][14] =
-    {
-        {0x29,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x2D,0x2E,0x2A},
-        {0x2B,0x14,0x1A,0x08,0X15,0X17,0X1C,0X18,0X0C,0X12,0X13,0X2F,0X30,0X48},
-        {0x39,0X04,0X16,0X07,0X09,0X0A,0X0B,0X0D,0X0E,0X0F,0X33,0X34,0X28,0X4C},
-        {0xE1,0X31,0X1D,0X1B,0X06,0X19,0X05,0X11,0X10,0X36,0X37,0X38,0X32,0X52},
-        {0xE0,0XE3,0XE2,0X00,0X00,0X00,0X2C,0X00,0X00,0XE6,FUNC_KEY,0X50,0X51,0X4F}
-    };
-    const unsigned char fn_keymap[5][14] =
-    {
-        {0x00,0X3A,0X3B,0X3C,0X3D,0X3E,0X3F,0X40,0X41,0X42,0X43,0X44,0X45,0x00},
-        {0x2B,0x14,0x1A,0x08,0X15,0X17,0X1C,0X18,0X0C,0X12,0X13,0X2F,0X30,0X48},
-        {0x39,0X04,0X16,0X07,0X09,0X0A,0X0B,0X0D,0X0E,0X0F,0X33,0X34,0X28,0X4C},
-        {0xE1,0X31,0X1D,0X1B,0X06,0X19,0X05,0X11,0X10,0X36,0X37,0X38,0X32,0X52},
-        {0xE0,0XE3,0XE2,0X00,0X00,0X00,0X2C,0X00,0X00,0XE6,FUNC_KEY,0X50,0X51,0X4F}
-    };
-    // bool to state whether function key is pressed
-    const unsigned char
-    bool fning = false;
-
-    // define key outputs
-    uint key_out[14][5] = {KEY_RELEASED};
-    // define key debounce counter array and number of checks until a FLIP
-    uint key_debounce[14][5] = {0};
-    // gpio pin nums of keyboard columns starting from COL_0, COL_1, ...
-    uint cols[14] = {13,14,15,12,11,10,9,8,2,3,4,5,6,7};
-    // gpio pin nums of keyboard rows starting from ROW_0, ROW_2,...
-    uint rows[5] = {20,19,18,17,16};
-
-    // init gpio pins and set directions
-    for(int i = 0; i < 14; i++){
-        gpio_init(cols[i]);
-        gpio_set_dir(cols[i], false);
-        gpio_pull_up(cols[i]);
-    }
-    for(int i = 0; i < 5; i++){
-        gpio_init(rows[i]);
-        gpio_set_dir(rows[i], false);
-        gpio_pull_up(rows[i]);
-    }
-
-    while (true) {
-        for(int i = 0; i < 14; i++){
-            // send signal from column i
-            gpio_set_dir(cols[i], true);
-            gpio_put(cols[i], 0);
-
-            for(int j = 0; j < 5; j++){
-                // read row j: if it is 0 and the debounce on the key exceeds FLIP, then i,j is pressed
-                bool read = gpio_get(rows[j]);
-                if(read == KEY_PRESSED && key_out[i][j] == KEY_RELEASED){
-                    if(key_debounce[i][j] >= FLIP){
-                        // key has been confirmed to be pressed
-                        // check and set if func key pressed and send to event queue that released all keys
-                        // fetch key value at key_out location and send to event queue that it has been pressed
-                        if(keymap[j][i] == FUNC_KEY){
-                            fning = true;
-                            for(int y = 0; y < 14; y++){
-                                for(int x = 0; x < 5; x++){
-                                    key_out[y][x] = KEY_RELEASED;
-                                    printf("%x\n", keymap[x][y]);
-                                }
-                            }
-                        }
-                        else if(fning){
-                            printf("%x\n", fn_keymap[j][i]);
+        if(timer_fired){
+            //? queue the chars that are currently pressed and ready the report
+            for(int col = 0; col < 14; col++){
+                for(int row = 0; row < 5; row++){
+                    if(pressed_keys[row][col] == 1){
+                        if(pressed_keys[4][10]){
+                            queue_character(fn_keymap[row][col]);
                         }
                         else{
-                            printf("%x\n", keymap[j][i]);
+                            queue_character(keymap[row][col]);
                         }
-                        key_out[i][j] = KEY_PRESSED;
-                        key_debounce[i][j] = MAX_DEBOUNCE;
                     }
-                    else{
-                        // increment debounce state
-                        key_debounce[i][j] ++;
-                    }
-                }
-                else if(read == KEY_RELEASED && key_out[i][j] == KEY_PRESSED){
-                    if(key_debounce[i][j] < FLIP){
-                        // key has been confirmed to be released
-                        // check and set if func key pressed
-                        // fetch key value at key_out location and send to event queue that it has been released
-                        if(keymap[j][i] == FUNC_KEY){
-                            fning = false;
-                        }
-                        else if(fning){
-                            printf("%x\n", fn_keymap[j][i]);
-                        }
-                        else{
-                            printf("%x\n", keymap[j][i]);
-                        }
-                        key_out[i][j] = KEY_RELEASED;
-                        key_debounce[i][j] = 0;
-                    }
-                    else{
-                        // decrement debounce state
-                        key_debounce[i][j] --;
-                    }
-                }
-                else if(key_debounce[i][j] > 0 && key_out[i][j] == KEY_RELEASED){
-                    // current read is same as key_out for i,j - return towards 0 debounce counts
-                    key_debounce[i][j] --;
-                }
-                else if(key_debounce[i][j] < MAX_DEBOUNCE && key_out[i][j] == KEY_PRESSED){
-                    // current read is same as key_out for i,j - return towards the MAX_DEBOUNCE debounce counts
-                    key_debounce[i][j] ++;
                 }
             }
-            // turn off signal from column i
-            gpio_set_dir(cols[i], false);
+            // queue_character(0x09);
+            report_ready();
+            //? add a new alarm
+            add_alarm_in_ms(REPORT_DELAY_MS, report_alarm_callback, NULL, false);
         }
-        // send queued keys to usb
-
     }
+
     return 0;
 }
-*/
+
+//! scanning column then row is not the most memory efficient
